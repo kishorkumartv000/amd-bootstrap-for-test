febf9a9ca6bdf1d8abc7f68c26ad28ee0c679c9d
Neon Light <kishorkumartv000@gmail.com>
2025-09-09 12:30:59 +0530

    Update uninstall_GO_and_am.sh

--- GO Updates/ungo.sh
+++ GO Updates/ungo.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 echo "=== Uninstalling Go and Apple Music Downloader ==="
 
-# Remove Go installation directory
+# Configuration
+CUSTOM_BINARY_NAME=am_downloader
+GO_BIN="$HOME/go-sdk/go/bin"
+
+# Remove system-wide Go installation
 sudo rm -rf /usr/local/go
 
-# Remove any Go-related symlinks or binaries (if manually linked)
+# Remove local Go installation
+rm -rf "$HOME/go-sdk"
+
+# Remove Go-related symlinks (fallback and manual)
 sudo rm -f /usr/bin/go /usr/bin/gofmt
+sudo rm -f /usr/local/bin/go /usr/local/bin/gofmt
+
+# Remove custom binary if installed globally
+sudo rm -f /usr/local/bin/$CUSTOM_BINARY_NAME
 
 # Remove Go workspace cache and mod cache
 rm -rf ~/go/pkg/mod
@@ -15,6 +26,8 @@ rm -rf ~/go/src
 # Remove Go path from shell config
 sed -i '/\/usr\/local\/go\/bin/d' ~/.bashrc
 sed -i '/\/usr\/local\/go\/bin/d' ~/.zshrc
+sed -i "\|$GO_BIN|d" ~/.bashrc
+sed -i "\|$GO_BIN|d" ~/.zshrc
 
 # Reload shell config
 source ~/.bashrc 2>/dev/null || source ~/.zshrc 2>/dev/null
