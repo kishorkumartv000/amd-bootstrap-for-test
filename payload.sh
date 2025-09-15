bc2e1f854d9b7b3c04e9e300255a828f324fcc0b
Neon Light <kishorkumartv000@gmail.com>
2025-09-15 15:53:56 +0530

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -10,7 +10,7 @@ TARGET_DIR2="/usr/src/app/tidal-dl-ng"
 WORKING_DIR="/usr/src/app"
 LOG_FILE="/var/log/payload.log"
 
-INSTALL_DEPS_REPO1=false
+INSTALL_DEPS_REPO1=true
 INSTALL_DEPS_REPO2=false
 INSTALL_POETRY=false
 
